24939a16b90955fdec29768e948cbd2213fb83e9
Yana-Krukovets <yanashelli@rambler.ru>
2013-04-28 14:32:54 +0300

    Add scroll

--- Information_analysis_of_university/MainForm.Designer.cs
+++ Information_analysis_of_university/MainForm.Designer.cs
@@ -55,13 +55,15 @@
             this.удалитьМодельToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.tcModelsFrame = new System.Windows.Forms.TabControl();
             this.newTabPage1 = new System.Windows.Forms.TabPage();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.button10 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.menuStrip1.SuspendLayout();
             this.menuBox1.SuspendLayout();
             this.contextMenuStrip1.SuspendLayout();
             this.tcModelsFrame.SuspendLayout();
             this.newTabPage1.SuspendLayout();
+            this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -304,7 +306,7 @@
             // 
             // newTabPage1
             // 
-            this.newTabPage1.Controls.Add(this.pictureBox1);
+            this.newTabPage1.Controls.Add(this.panel1);
             this.newTabPage1.Location = new System.Drawing.Point(4, 22);
             this.newTabPage1.Name = "newTabPage1";
             this.newTabPage1.Padding = new System.Windows.Forms.Padding(3);
@@ -312,16 +314,6 @@
             this.newTabPage1.TabIndex = 0;
             this.newTabPage1.Text = "new1";
             this.newTabPage1.UseVisualStyleBackColor = true;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(2365, 1166);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
-            this.pictureBox1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDoubleClick);
             // 
             // button10
             // 
@@ -331,6 +323,25 @@
             this.button10.TabIndex = 8;
             this.button10.Text = "Справка";
             this.button10.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(790, 372);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(3, 22);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1280, 872);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
             // 
             // MainForm
             // 
@@ -353,6 +364,7 @@
             this.contextMenuStrip1.ResumeLayout(false);
             this.tcModelsFrame.ResumeLayout(false);
             this.newTabPage1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -376,7 +388,6 @@
         private System.Windows.Forms.TabControl tcModelsFrame;
         private System.Windows.Forms.TabPage newTabPage1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
@@ -389,6 +400,8 @@
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
